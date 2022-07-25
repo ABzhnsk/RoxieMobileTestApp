7cee82725bf5559bc3472d394d9f97d155f12dc1
ABzhnsk <96691110+ABzhnsk@users.noreply.github.com>
2022-07-25 15:34:11 +0300

    Create class NetworkService

---
+++ RoxieMobileTestApp/RoxieMobileTestApp/Services/NetworkService.swift
@@ -0,0 +1,18 @@
+//
+//  NetworkService.swift
+//  RoxieMobileTestApp
+//
+//  Created by Anna Buzhinskaya on 25.07.2022.
+//
+
+import Foundation
+
+class NetworkService {
+    static func getOrders() {
+        
+    }
+    
+    static func getImages() {
+        
+    }
+}
