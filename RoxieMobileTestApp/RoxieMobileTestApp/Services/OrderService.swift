e6c17768de256b98117e898551ca95ae6c643b6d
ABzhnsk <96691110+ABzhnsk@users.noreply.github.com>
2022-07-25 21:41:02 +0300

    Create class OrderService

---
+++ RoxieMobileTestApp/RoxieMobileTestApp/Services/OrderService.swift
@@ -0,0 +1,22 @@
+//
+//  OrderService.swift
+//  RoxieMobileTestApp
+//
+//  Created by Anna Buzhinskaya on 25.07.2022.
+//
+
+import Foundation
+import UIKit
+
+class OrderService {
+    static var ordersInfo = [OrderInfo]()
+    static var ordersImage = [UIImage]()
+    
+    static func loadOrders() {
+        
+    }
+    
+    static func loadOrdersImage() {
+        
+    }
+}
