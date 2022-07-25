788b29ba1da9a97202962d46fe336f8244554475
ABzhnsk <96691110+ABzhnsk@users.noreply.github.com>
2022-07-25 16:48:06 +0300

    Test request
    Using JSONSerialization

--- RoxieMobileTestApp/RoxieMobileTestApp/Controllers/StartViewController.swift
+++ RoxieMobileTestApp/RoxieMobileTestApp/Controllers/StartViewController.swift
@@ -11,7 +11,7 @@ class StartViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        NetworkService.getOrders()
     }
 
 }
--- RoxieMobileTestApp/RoxieMobileTestApp/Services/NetworkService.swift
+++ RoxieMobileTestApp/RoxieMobileTestApp/Services/NetworkService.swift
@@ -6,11 +6,24 @@
 //
 
 import Foundation
+import Alamofire
 
 class NetworkService {
     static func getOrders() {
         
-    }
+        AF
+            .request("https://www.roxiemobile.ru/careers/test/orders.json")
+            .responseData { response in
+                switch response.result{
+                case .success(let data):
+                    let json = try? JSONSerialization.jsonObject(with: data,
+                                                                 options: JSONSerialization.ReadingOptions.fragmentsAllowed)
+                    print(json)
+                case .failure(let error):
+                    print(error)
+                }
+            }
+        }
     
     static func getImages() {
         
