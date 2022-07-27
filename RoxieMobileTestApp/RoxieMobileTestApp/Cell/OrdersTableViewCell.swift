96a92f7e9f2a656534ce86eb35b5dcc4069acc3b
ABzhnsk <96691110+ABzhnsk@users.noreply.github.com>
2022-07-27 22:34:28 +0300

    Customize cell's label

--- RoxieMobileTestApp/RoxieMobileTestApp/Cell/OrdersTableViewCell.swift
+++ RoxieMobileTestApp/RoxieMobileTestApp/Cell/OrdersTableViewCell.swift
@@ -14,11 +14,20 @@ class OrdersTableViewCell: UITableViewCell {
     @IBOutlet weak var dateOrder: UILabel!
     @IBOutlet weak var amountOrder: UILabel!
     
+    static let dateFormatter: DateFormatter = {
+        let df = DateFormatter()
+        df.dateFormat = "dd.MM.yy"
+        return df
+    }()
+    
     func configure(withData order: OrderInfo) {
-        self.startAddress.text = order.startAddress.address
-        self.endAddress.text = order.endAddress.address
-        self.dateOrder.text = order.orderTime
-        self.amountOrder.text = String(order.price.amount)
+        let dateFormatter = ISO8601DateFormatter()
+        guard let date = dateFormatter.date(from: order.orderTime) else {return}
+        let stringDate = OrdersTableViewCell.dateFormatter.string(from: date)
+        self.startAddress.text = "Откуда: \(order.startAddress.address)"
+        self.endAddress.text = "Куда: \(order.endAddress.address)"
+        self.dateOrder.text = "Дата: \(stringDate)"
+        self.amountOrder.text = "Стоимость: \(String(order.price.amount) + order.price.currency)"
     }
     
     override func prepareForReuse() {
