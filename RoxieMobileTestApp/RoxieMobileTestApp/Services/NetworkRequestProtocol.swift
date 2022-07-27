511ac7fede7d77b8a23abbef5e9040926eac7fac
ABzhnsk <96691110+ABzhnsk@users.noreply.github.com>
2022-07-27 18:22:12 +0300

    Add extension for protocol NetworkRequest
    add HTTP request using Alamofire

--- RoxieMobileTestApp/RoxieMobileTestApp/Services/NetworkRequestProtocol.swift
+++ RoxieMobileTestApp/RoxieMobileTestApp/Services/NetworkRequestProtocol.swift
@@ -6,9 +6,24 @@
 //
 
 import Foundation
+import Alamofire
 
 protocol NetworkRequest: AnyObject {
     associatedtype ModelType
     func decode(_ data: Data) -> ModelType?
     func execute(withCompletion completion: @escaping (ModelType?) -> Void)
+}
+
+extension NetworkRequest {
+    func load(_ url: URL, withCompletion completion: @escaping (ModelType?) -> Void) {
+        AF
+            .request(url, method: .get)
+            .responseData { [weak self] response in
+                guard let data = response.value else {
+                    DispatchQueue.main.async { completion(nil) }
+                    return
+                }
+                DispatchQueue.main.async { completion(self?.decode(data)) }
+            }
+    }
 }
