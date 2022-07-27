0b86e27e2e1b457a9108098175d3c55486593b27
ABzhnsk <96691110+ABzhnsk@users.noreply.github.com>
2022-07-27 18:32:42 +0300

    Update OrderInfo
    Change struct OrderInfo to Decodable and removing SwiftyJSON

--- RoxieMobileTestApp/RoxieMobileTestApp/Model/OrderInfo.swift
+++ RoxieMobileTestApp/RoxieMobileTestApp/Model/OrderInfo.swift
@@ -6,34 +6,30 @@
 //
 
 import Foundation
-import SwiftyJSON
 
-struct OrderInfo {
+
+struct OrderInfo: Decodable {
     let id: Int
-    let startCity: String
-    let startAddress: String
-    let endCity: String
-    let endAddress: String
+    let startAddress: Address
+    let endAddress: Address
+    let price: Price
+    let orderTime: String
+    let vehicle: Vehicle
+}
+
+struct Address: Decodable {
+    let city: String
+    let address: String
+}
+
+struct Price: Decodable {
     let amount: Int
     let currency: String
-    let orderTime: String
+}
+
+struct Vehicle: Decodable {
     let regNumber: String
     let modelName: String
     let photo: String
     let driverName: String
-    
-    init(_ json: JSON){
-        self.id = json["id"].intValue
-        self.startCity = json["startAddress"]["city"].stringValue
-        self.startAddress = json["startAddress"]["address"].stringValue
-        self.endCity = json["endAddress"]["city"].stringValue
-        self.endAddress = json["endAddress"]["address"].stringValue
-        self.amount = json["price"]["amount"].intValue
-        self.currency = json["price"]["currency"].stringValue
-        self.orderTime = json["orderTime"].stringValue
-        self.regNumber = json["vehicle"]["regNumber"].stringValue
-        self.modelName = json["vehicle"]["modelName"].stringValue
-        self.photo = json["vehicle"]["photo"].stringValue
-        self.driverName = json["vehicle"]["driverName"].stringValue
-    }
 }
