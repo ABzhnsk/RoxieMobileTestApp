abeca94c3eaa41e04b456f9bb6cd6974e996969d
ABzhnsk <96691110+ABzhnsk@users.noreply.github.com>
2022-07-27 19:36:02 +0300

    Add logic in StartViewController

--- RoxieMobileTestApp/RoxieMobileTestApp/Controllers/StartViewController.swift
+++ RoxieMobileTestApp/RoxieMobileTestApp/Controllers/StartViewController.swift
@@ -8,10 +8,23 @@
 import UIKit
 
 class StartViewController: UIViewController {
-
+    private var orders = [OrderInfo]()
+    private var request: APIRequest<OrdersResource>?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        fetchOrders()
     }
+}
 
+extension StartViewController {
+    func fetchOrders() {
+        let orderRequest = APIRequest(resource: OrdersResource())
+        request = orderRequest
+        orderRequest.execute { [weak self] (orders: [OrderInfo]?) in
+            guard let orders = orders else { return }
+            self?.orders = orders
+        }
+    }
 }
