a4175560868e0db37874f5271778283d1a499919
ABzhnsk <96691110+ABzhnsk@users.noreply.github.com>
2022-07-27 20:21:52 +0300

    Add tableView logic

--- RoxieMobileTestApp/RoxieMobileTestApp/Controllers/StartViewController.swift
+++ RoxieMobileTestApp/RoxieMobileTestApp/Controllers/StartViewController.swift
@@ -12,10 +12,15 @@ class StartViewController: UIViewController {
     
     private var orders = [OrderInfo]()
     private var request: APIRequest<OrdersResource>?
+    private let reuseIdentifier = "reuseIdentifier"
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        tableView.register(UINib(nibName: "OrdersTableViewCell", bundle: nil),
+                           forCellReuseIdentifier: reuseIdentifier)
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.reloadData()
         fetchOrders()
     }
 }
@@ -27,6 +32,27 @@ extension StartViewController {
         orderRequest.execute { [weak self] (orders: [OrderInfo]?) in
             guard let orders = orders else { return }
             self?.orders = orders
+            self?.tableView.reloadData()
         }
+    }
+}
+
+extension StartViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return orders.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier,
+                                                       for: indexPath) as? OrdersTableViewCell
+        else { return UITableViewCell() }
+        cell.configure(withData: orders[indexPath.row])
+        return cell
+    }
+}
+
+extension StartViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 130
     }
 }
